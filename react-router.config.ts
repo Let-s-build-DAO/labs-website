c23331cfe9abcf830d26ed20d5bc00a3770199c8
iamalaboexcel <iamalaboexcel@gmail.com>
2025-08-03 13:36:44 +0100

    deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  // Enable static site generation (prerendering)
   ssr: true,
+  prerender: ["/"], // Prerender the home page to static HTML
 } satisfies Config;
